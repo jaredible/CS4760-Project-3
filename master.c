8dc110701e96f1027c382f4698dfde416339cba2
Jared Diehl <diehl@hoare7.cs.umsl.edu>
2020-10-12 23:04:54 -0500

    Added comments

--- master.c
+++ master.c
@@ -1,8 +1,12 @@
 /*
  * master.c 10/16/20
  * Jared Diehl (jmddnb@umsystem.edu)
+ * -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+ * This program creates palin processes
+ * to process the input file's strings.
  */
 
+/* Standard library header files */
 #include <ctype.h> // isdigit
 #include <getopt.h> // getopt, optarg, optind
 #include <signal.h> // signal
@@ -19,10 +23,15 @@
 #include <sys/wait.h> // wait
 #include <unistd.h> // F_OK, access, execl, fork
 
+/* Programmer-defined header files */
 #include "constant.h"
 #include "helper.h"
 #include "shared.h"
 
+/* Definitions */
+#define OPTIONS "hs:t:"
+
+/* Private function prototypes */
 static void usage(int);
 static int load(char*);
 static void spawn(int);
@@ -30,30 +39,36 @@ static void timer(int);
 static void handler(int);
 static void finalize(bool);
 
+/* Private global variables */
 static int n = TOTAL_CHILDREN_DEFAULT;
 static int s = CONCURRENT_CHILDREN_DEFAULT;
 static int t = TIMEOUT_DEFAULT;
 
-int main(int argc, char** argv) {
+/* Main program */
+int main(int argc, char **argv) {
 	init(argc, argv);
 	
 	bool ok = true;
 	
+	/* Get options */
 	while (true) {
-		int c = getopt(argc, argv, "hs:t:");
+		int c = getopt(argc, argv, OPTIONS);
 		
 		if (c == -1) break;
 		
 		switch (c) {
 			case 'h':
+				/* Display usage information */
 				usage(EXIT_SUCCESS);
 			case 's':
+				/* Ensure option s is valid and within range */
 				if (!isdigit(*optarg) || (s = atoi(optarg)) < 0) {
 					error("invalid concurrent children '%s'", optarg);
 					ok = false;
 				}
 				break;
 			case 't':
+				/* Ensure option t is valid and within range */
 				if (!isdigit(*optarg) || (t = atoi(optarg)) < 0) {
 					error("invalid timeout '%s'", optarg);
 					ok = false;
@@ -64,8 +79,10 @@ int main(int argc, char** argv) {
 		}
 	}
 	
-	char *path = NULL;
+	/* Path of input file */
+	char *path;
 	
+	/* Check input file */
 	if ((path = argv[optind]) == NULL) {
 		error("no argument supplied for infile");
 		ok = false;
@@ -76,47 +93,57 @@ int main(int argc, char** argv) {
 	
 	if (!ok) usage(EXIT_FAILURE);
 	
+	/* Register Ctrl+C signal handler */
 	sigact(SIGINT, &handler);
 	
+	/* Initialize output files */
 	rtouch("output.log");
 	rtouch("palin.out");
 	rtouch("nopalin.out");
 	
+	/* Initialize shared memory and semaphores */
 	shmAllocate(true);
-	semAllocate(true);
+	semAllocate(true, 3);
 	
+	/* Output the options */
 	char msg[BUFFER_LENGTH];
-	strfcpy(msg, "%s: Starting with options: s=%d, t=%d\n", ftime(), s, t);
+	sprintf(msg, "%s: Starting with options: s=%d, t=%d\n", ftime(), s, t);
 	fprintf(stderr, msg);
 	flog("output.log", msg);
 	
+	/* Initialize strings */
 	int c = load(path);
 	
-	strfcpy(msg, "%s: Processing %d strings\n", ftime(), c);
+	/* Output the string count */
+	sprintf(msg, "%s: Processing %d strings\n", ftime(), c);
 	fprintf(stderr, msg);
 	flog("output.log", msg);
 	
+	/* Initialize timer */
 	if (t == 0) {
 		finalize(true);
 		exit(EXIT_SUCCESS);
 	} else timer(t);
 	
+	/* Clamp the options */
 	n = MIN(c, CHILD_COUNT);
 	s = MIN(s, n);
 	
-	int i = 0;
-	int j = n;
+	/* Temporary variables */
+	int i, j, status;
 	
-	while (i < s)
-		spawn(i++);
+	/* Spawn initial children */
+	for (i = 0; i < s; i++)
+		spawn(i);
 	
-	int status;
-	
-	while (j > 0 && s > 0) {
+	/* Keep trying to spawn children after one dies */
+	for (j = n; j > 0 && s > 0; j--) {
+		/* Wait for a child to die */
 		wait(&status);
+		/* Output that a child process is finished */
 		if (WIFEXITED(status)) flog("output.log", "%s: Process %d finished\n", ftime(), WEXITSTATUS(status));
+		/* Attempt to spawn a child */
 		if (i < n) spawn(i++);
-		j--;
 	}
 	
 	finalize(true);
@@ -124,9 +151,17 @@ int main(int argc, char** argv) {
 	return ok ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
+/*
+ * usage(status)
+ * -------------
+ * Displays a help message or usage information depending on status, and exits.
+ */
 static void usage(int status) {
-	if (status != EXIT_SUCCESS) fprintf(stderr, "Try '%s -h' for more information\n", getProgramName());
-	else {
+	if (status != EXIT_SUCCESS) {
+		/* Display help message */
+		fprintf(stderr, "Try '%s -h' for more information\n", getProgramName());
+	} else {
+		/* Display usage information */
 		printf("NAME\n");
 		printf("       %s - palindrome finder\n", getProgramName());
 		printf("USAGE\n");
@@ -140,7 +175,13 @@ static void usage(int status) {
 	exit(status);
 }
 
+/*
+ * load(path)
+ * ----------
+ * From the file specified by path, loads into shared memory all its strings.
+ */
 static int load(char *path) {
+	/* Attempt to open the file */
 	FILE *fp;
 	if ((fp = fopen(path, "r")) == NULL) crash("fopen");
 	
@@ -148,9 +189,11 @@ static int load(char *path) {
 	char *line;
 	size_t len = 0;
 	ssize_t read;
-	while (i < n && (read = getline(&line, &len, fp)) != -1) {
-		crnl(line);
-		setString(i++, line);
+	
+	/* Copy every line with a line index less than n into shared memory */
+	for (; i < n && (read = getline(&line, &len, fp)) != -1; i++) {
+		crnl(line); /* Remove newline character */
+		setString(i, line); /* Copy line into shared memory at index i*/
 	}
 	
 	if (fclose(fp) == EOF) crash("fclose");
@@ -159,23 +202,41 @@ static int load(char *path) {
 	return i;
 }
 
+/*
+ * spawn(index)
+ * ------------
+ * Creates a new child to execute the palin program.
+ */
 static void spawn(int index) {
-	int cpid = fork();
+	/* Attempt to create a new child process */
+	pid_t cpid = fork();
 	if (cpid == -1) crash("fork");
 	else if (cpid == 0) {
+		/* Set this new process' PGID */
 		if (index == 0) setCpgid(getpid());
 		setpgid(0, getCpgid());
+		
+		/* Output that a child process is starting */
 		flog("output.log", "%s: Process %d starting\n", ftime(), index);
-		char cindex[3];
+		
+		/* Execute palin program */
+		char cindex[BUFFER_LENGTH];
 		sprintf(cindex, "%d", index);
 		execl("./palin", "palin", cindex, (char*) NULL);
+		
 		exit(EXIT_SUCCESS);
 	}
 }
 
+/* timer(seconds)
+ * --------------
+ * Sets up the timer signal handler.
+ */
 static void timer(int seconds) {
+	/* Register timer signal handler */
 	sigact(SIGALRM, &handler);
 	
+	/* Initialize real timer */
 	struct itimerval itv;
 	itv.it_value.tv_sec = seconds;
 	itv.it_value.tv_usec = 0;
@@ -184,22 +245,39 @@ static void timer(int seconds) {
 	if (setitimer(ITIMER_REAL, &itv, NULL) == -1) crash("setitimer");
 }
 
-static void handler(int signum) {
+/*
+ * handler(signal)
+ * ---------------
+ * Handles all programmer-defined signals.
+ */
+static void handler(int signal) {
+	/* Output the caught signal */
 	char msg[BUFFER_LENGTH];
-	strfcpy(msg, "%s: Signal %s caught\n", ftime(), signum == SIGALRM ? "timeout" : "interrupt");
+	sprintf(msg, "%s: Signal %s caught\n", ftime(), signal == SIGALRM ? "timeout" : "interrupt");
 	fprintf(stderr, msg);
 	flog("output.log", msg);
-	killpg(getCpgid(), signum == SIGALRM ? SIGUSR1 : SIGTERM);
+	
+	/* Kill all children */
+	killpg(getCpgid(), signal == SIGALRM ? SIGUSR1 : SIGTERM);
 	while (wait(NULL) > 0);
+	
 	finalize(false);
 	exit(EXIT_SUCCESS);
 }
 
+/*
+ * finalize(finished)
+ * ------------------
+ * Outputs an exit message and releases resources.
+ */
 static void finalize(bool finished) {
+	/* Output exit status */
 	char msg[BUFFER_LENGTH];
-	strfcpy(msg, "%s: Exiting %sfinished\n", ftime(), finished ? "" : "un");
+	sprintf(msg, "%s: Exiting %sfinished\n", ftime(), finished ? "" : "un");
 	fprintf(stderr, msg);
 	flog("output.log", msg);
+	
+	/* Release shared memory and semaphores */
 	shmRelease();
 	semRelease();
 }
--- palin.c
+++ palin.c
@@ -3,63 +3,84 @@
  * Jared Diehl (jmddnb@umsystem.edu)
  */
 
+/* Standard library header files */
 #include <ctype.h> // tolower
-#include <signal.h>
+#include <signal.h> // SIGTERM, SIGUSR1
 #include <stdbool.h> // false, true
 #include <stdio.h> // printf
 #include <stdlib.h> // EXIT_SUCCESS
 #include <string.h> // strlen
-#include <sys/sem.h>
 #include <time.h> // time
 #include <unistd.h> // sleep
 
+/* Programmer-defined header files */
 #include "constant.h"
 #include "helper.h"
 #include "shared.h"
 
+/* Private function prototypes */
 static bool palindrome(char*);
 static void handler(int);
 
+/* Private global variables */
 static int cindex;
 
-int main(int argc, char** argv) {
+/* Main program */
+int main(int argc, char **argv) {
 	init(argc, argv);
 	
+	/* Register signal handlers */
 	sigact(SIGTERM, &handler);
 	sigact(SIGUSR1, &handler);
 	
+	/* Check and get option */
 	if (argc < 2) error("no argument supplied for index");
 	else cindex = atoi(argv[1]);
 	
+	/* Initialize random's seed */
 	srand(time(NULL) + getpid() * cindex);
 	
+	/* Attach to shared memory and semaphores */
 	shmAllocate(false);
 	semAllocate(false);
 	
+	/* Check if string is a palindrome */
 	char *string = getString(cindex);
 	bool is = palindrome(string);
-
+	
 	if (DEBUG) printf("%s: Process %d found that %s is %sa palindrome\n", ftime(), cindex, string, is ? "" : "not ");
 	
+	/* Display that this process wants to enter the critical section */
 	fprintf(stderr, "%s: Process %d wants to enter critical section\n", ftime(), cindex);
 	
+	/* Lock semaphore */
 	semWait(is ? 0 : 1);
 	
 	/* Enter critical section */
 	
+	/* Display that this process is in the critical section */
 	fprintf(stderr, "%s: Process %d in critical section\n", ftime(), cindex);
+	/* Sleep for 0-2 seconds */
 	sleep(rand() % 3);
+	/* Output the string to the appropriate output file */
 	flog(is ? "palin.out" : "nopalin.out", "%s %d %d %s\n", ftime(), getpid(), cindex, string);
+	/* Display that this process is exiting the critical section */
 	fprintf(stderr, "%s: Process %d exiting critical section\n", ftime(), cindex);
 	
 	/* Exit critical section */
 	
+	/* Unlock semaphore */
 	semSignal(is ? 0 : 1);
 	
+	/* Exit with the process' index */
 	return cindex;
 }
 
-
+/*
+ * palindrome(string)
+ * ------------------
+ * Returns if the given string is a palindrome.
+ */
 static bool palindrome(char *string) {
 	int li, ri;
 	char lc, rc;
@@ -76,12 +97,19 @@ static bool palindrome(char *string) {
 	return true;
 }
 
-static void handler(int signum) {
+/*
+ * handler(signal)
+ * ---------------
+ * Handles all programmer-defined signals.
+ */
+static void handler(int signal) {
+	/* Output the caught status and exit */
 	semWait(2);
 	char msg[BUFFER_LENGTH];
-	strfcpy(msg, "%s: Process %d exiting due to %s signal\n", ftime(), cindex, signum == SIGUSR1 ? "timeout" : "interrupt");
+	sprintf(msg, "%s: Process %d exiting due to %s signal\n", ftime(), cindex, signal == SIGUSR1 ? "timeout" : "interrupt");
 	fprintf(stderr, msg);
 	flog("output.log", msg);
 	semSignal(2);
+
 	exit(EXIT_FAILURE);
 }
