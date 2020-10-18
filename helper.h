97d3b0f395f9acdf4f41dd2210c99f053f8b4578
Jared Diehl <diehl@hoare7.cs.umsl.edu>
2020-10-17 22:31:46 -0500

    Forget to remove prototypes

--- helper.h
+++ helper.h
@@ -11,12 +11,10 @@
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 
-void error(char*, ...);
 void sigact(int, void(int));
 void crnl(char*);
 void flog(char*, char*, ...);
 char *ftime();
 void rtouch(char*);
-void crash(char*);
 
 #endif
